503deec3d8607345bc8e5431b312acb06ffeb439
Laleth R <107640647+Laleth005@users.noreply.github.com>
2025-02-04 21:11:18 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
     echo hi123
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u laleth2614 -p laleth@2614
-    docker tag test laleth2614/ar
-    docker push laleth2614/ar
+    docker login -u laleth -p laleth@2614
+    docker tag test laleth/task
+    docker push laleth/task
     
